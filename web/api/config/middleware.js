291b688a935eddd8cc99093f1d51e7b198fc1b1c
LiiNen <kjeonghoon065@gmail.com>
2020-10-05 14:14:56 +0000

    Update: API & DB system

--- web/api/config/middleware.js
+++ web/api/config/middleware.js
@@ -7,6 +7,8 @@ module.exports = {
         "https://www.kookbang.kr",
         "https://api.kookbang.kr",
         "https://friends.kookbang.kr",
+        "https://osam.kookbang.kr",
+        "https://osam2.kookbang.kr",
       ],
     },
   },
